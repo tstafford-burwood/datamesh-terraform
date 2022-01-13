f9162f8135f44179eb29a20bc04604fc4380deee
Aaron Strong <astrong@burwood.com>
2022-01-12 20:40:56 -0600

    removed vpc sc

--- environment/deployments/data-lake/outputs.tf
+++ environment/deployments/data-lake/outputs.tf
@@ -65,20 +65,20 @@ output "staging_data_lake_ingress_bq_dataset" {
 # VPC SC REGULAR SERVICE PERIMETER - DATA LAKE OUTPUTS
 #-----------------------------------------------------
 
-output "regular_service_perimeter_resources" {
-  description = "A list of GCP resources that are inside of the service perimeter. Currently only projects are allowed."
-  value       = module.data_lake_regular_service_perimeter.regular_service_perimeter_resources
-}
+# output "regular_service_perimeter_resources" {
+#   description = "A list of GCP resources that are inside of the service perimeter. Currently only projects are allowed."
+#   value       = module.data_lake_regular_service_perimeter.regular_service_perimeter_resources
+# }
 
-output "regular_service_perimeter_name" {
-  description = "The perimeter's name."
-  value       = module.data_lake_regular_service_perimeter.regular_service_perimeter_name
-}
+# output "regular_service_perimeter_name" {
+#   description = "The perimeter's name."
+#   value       = module.data_lake_regular_service_perimeter.regular_service_perimeter_name
+# }
 
-output "vpc_accessible_services" {
-  description = "The API services accessible from a network within the VPC SC perimeter."
-  value       = module.data_lake_regular_service_perimeter.vpc_accessible_services
-}
+# output "vpc_accessible_services" {
+#   description = "The API services accessible from a network within the VPC SC perimeter."
+#   value       = module.data_lake_regular_service_perimeter.vpc_accessible_services
+# }
 
 #-----------------------------
 # VPC SC ACCESS LEVELS OUTPUTS

460501fda476db8678fc5f5a98f1fd5122169dcc
dspeck1 <dspeck@burwood.com>
2022-01-19 16:12:46 -0600

    added outputs for workflow

---
+++ environment/foundation/cloudbuild-sde/outputs.tf
@@ -0,0 +1,15 @@
+output "folders_apply_trigger_dev" {
+    value = google_cloudbuild_trigger.folders_apply_dev.id
+}
+
+output "packer_project_apply_trigger_dev" {
+    value = google_cloudbuild_trigger.packer_project_apply_dev.id
+}
+
+output "staging_project_apply_trigger_dev" {
+    value = google_cloudbuild_trigger.staging_project_apply_dev.id
+}
+
+output "data_lake_project_apply_trigger_dev" {
+    value = google_cloudbuild_trigger.data_lake_project_apply_dev.id
+}

a6752f672c2d0d917b33848875e74d118260c6b5
dspeck1 <dspeck@burwood.com>
2022-01-19 22:45:22 -0600

    updated for trigger id

--- environment/foundation/cloudbuild-sde/outputs.tf
+++ environment/foundation/cloudbuild-sde/outputs.tf
@@ -1,15 +1,15 @@
 output "folders_apply_trigger_dev" {
-    value = google_cloudbuild_trigger.folders_apply_dev.id
+    value = google_cloudbuild_trigger.folders_apply_dev.trigger_id
 }
 
 output "packer_project_apply_trigger_dev" {
-    value = google_cloudbuild_trigger.packer_project_apply_dev.id
+    value = google_cloudbuild_trigger.packer_project_apply_dev.trigger_id
 }
 
 output "staging_project_apply_trigger_dev" {
-    value = google_cloudbuild_trigger.staging_project_apply_dev.id
+    value = google_cloudbuild_trigger.staging_project_apply_dev.trigger_id
 }
 
 output "data_lake_project_apply_trigger_dev" {
-    value = google_cloudbuild_trigger.data_lake_project_apply_dev.id
+    value = google_cloudbuild_trigger.data_lake_project_apply_dev.trigger_id
 }

d636c3335473ad18892e9327823ace3182700bc7
dspeck1 <dspeck@burwood.com>
2022-01-26 09:41:58 -0600

    moved dir

---
+++ environment/foundation/vpc-service-controls/perimeter/README.md
@@ -0,0 +1,5 @@
+# VPC Service Controls
+
+[Egress Rules Reference](https://cloud.google.com/vpc-service-controls/docs/ingress-egress-rules#egress-rules-reference)
+
+[Method Restrictions](https://cloud.google.com/vpc-service-controls/docs/supported-method-restrictions)
--- environment/foundation/vpc-service-controls/perimeter/variables.tf
+++ environment/foundation/vpc-service-controls/perimeter/variables.tf
@@ -16,7 +16,6 @@ variable "data_ops_egress_project_numbers" {
   default     = []
 }
 
-
 variable "restricted_services" {
   description = "GCP services that are subject to the Service Perimeter restrictions. Must contain a list of services. For example, if storage.googleapis.com is specified, access to the storage buckets inside the perimeter must meet the perimeter's access restrictions."
   type        = list(string)

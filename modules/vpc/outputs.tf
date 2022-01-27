cb819be947b946adc7318f4a6a68cfe2b5955e88
Aaron Strong <astrong@burwood.com>
2022-01-27 10:55:58 -0600

    update with newer outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,6 +2,11 @@
 # STANDALONE VPC MODULE OUTPUTS
 #------------------------------
 
+output "network_id" {
+  description = "The ID of the VPC being created."
+  value       = module.vpc.id
+}
+
 output "network_name" {
   description = "The name of the VPC being created"
   value       = module.vpc.network_name
@@ -12,6 +17,11 @@ output "network_self_link" {
   value       = module.vpc.network_self_link
 }
 
+output "subnets" {
+  description = "A map with keys of form subnet_region/subnet_name and values being the outputs of the google_compute_subnetwork resources used to create corresponding subnets."
+  value       = module.vpc.subnets
+}
+
 output "subnets_names" {
   description = "The names of the subnets being created"
   value       = module.vpc.subnets_names
@@ -20,6 +30,16 @@ output "subnets_names" {
 output "subnets_self_links" {
   description = "The self-links of subnets being created"
   value       = module.vpc.subnets_self_links
+}
+
+output "subnets_regions" {
+  description = "The region where the subnets will be created."
+  value       = module.vpc.subnets_regions
+}
+
+output "subnets_secondary_ranges" {
+  description = "The secondary ranges associated with these subnets"
+  value       = module.vpc.subnets_secondary_ranges
 }
 
 output "route_names" {

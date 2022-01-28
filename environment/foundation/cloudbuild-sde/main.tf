0065791e888885b6c2f6635d809bb8fdbe1dd44a
Aaron Strong <astrong@burwood.com>
2022-01-28 15:41:38 -0600

    fix triggers

--- environment/foundation/cloudbuild-sde/main.tf
+++ environment/foundation/cloudbuild-sde/main.tf
@@ -810,8 +810,8 @@ resource "google_cloudbuild_trigger" "researcher_workspace_project_plan_prod" {
 
   substitutions = {
     _BUCKET              = local.terraform_state_bucket
-    _PREFIX              = format("%s/%s", var.terraform_deployments_state_prefix, var.env_name_dev)
-    _PREFIX_FOUNDATION   = format("%s/%s", var.terraform_foundation_state_prefix, var.env_name_dev)
+    _PREFIX              = format("%s/%s", var.terraform_deployments_state_prefix, var.env_name_prod)
+    _PREFIX_FOUNDATION   = format("%s/%s", var.terraform_foundation_state_prefix, var.env_name_prod)
     _TAG                 = var.terraform_container_version
     _TFVARS_FILE         = ""
     _COMPOSER_DAG_BUCKET = local.composer_gcs_bucket_prod

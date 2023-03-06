6b6b5e093a6a15b13a4163abdebefdf6bf1d9647
Aaron Strong <astrong@Burwood.com>
2023-03-06 15:42:33 -0600

    new values

--- environment/foundation/constants/constants.tf
+++ environment/foundation/constants/constants.tf
@@ -5,27 +5,27 @@ locals {
   constants = {
 
     // DOMAIN INFORMATION
-    org_id                     = "956334456591" # gcloud organizations list                             
-    billing_account_id         = "019D3A-6DC5E4-54D0C6"
-    sde_folder_id              = "396928138340"
-    automation_project_id      = "prod-clientit-automation-21012"
-    cloudbuild_service_account = "68129454576@cloudbuild.gserviceaccount.com"
-    terraform_state_bucket     = "terraform-state-sde-prod-7dkc"
+    org_id                     = "575228741867"         # gcloud organizations list                             
+    billing_account_id         = "01EF01-627C10-7CD2DF" # gcloud alpha billing accounts list
+    sde_folder_id              = "487372633138"
+    automation_project_id      = "github-actions-demos"
+    cloudbuild_service_account = "62218100388@cloudbuild.gserviceaccount.com"
+    terraform_state_bucket     = "terraform-state-ecc94f44d34edc8e"
 
     // USERS & GROUPS TO ASSIGN TO THE FOUNDATION PROJECTS
     // format: `user:user1@client.edu`, `group:admins@client.edu`, or `serviceAccount:my-app@appspot.gserviceaccount.com`
-    ingress-project-admins = ["user:user@burwood.com"]
-    image-project-admins   = ["user:tap145@client.edu", "user:user@burwood.com"]
-    data-lake-admins       = ["user:tap145@client.edu", "user:user@burwood.com"]
-    data-lake-viewers      = ["user:tap145@client.edu", "user:user@burwood.com"]
-    data-ops-admins        = ["user:tap145@client.edu", "user:user@burwood.com"]
+    ingress-project-admins = ["group:sde-centralit@prorelativity.com"]
+    image-project-admins   = ["group:sde-centralit@prorelativity.com"]
+    data-lake-admins       = ["group:sde-centralit@prorelativity.com"]
+    data-lake-viewers      = ["group:sde-centralit@prorelativity.com"]
+    data-ops-admins        = ["group:sde-centralit@prorelativity.com"]
 
     // ONLY USERS OR SERVICE ACCOUNTS TO ASSIGN TO VPC PERIMETER
     # `vpc_sc_admins` are added to an access context level which grants them full access to all restricted services for all foundation projects
     # `vpc_sc_stewards` are added to an access context level. This access level `sde_{env}_stewards`, has an allow rule in the VPC SC that grants this access level to use the storage api only in the data ingress project.
     #   this is to allow stewards to upload files to a bucket in the data ingress project.
 
-    vpc_sc_admins   = []
+    vpc_sc_admins = []
 
     // Default Location
     default_region = "us-central1"
@@ -34,7 +34,7 @@ locals {
     // Long running Branches. These need to match the branch names in Version Control Software.
     environment = {
       # <branch_name> = <environment_value>
-      main = "dev"
+      base-1 = "qa"
     }
   }
 }

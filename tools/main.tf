61da98e99e983ba36b6e7fb02e8bae6043b8f98b
Aaron Strong <astrong@Burwood.com>
2023-03-06 13:33:39 -0600

    remove unused files

--- tools/main.tf
+++
@@ -1,8 +0,0 @@
-data "http" "webui" {
-    url = "https://xfa0d78138fc714fcp-tp.appspot.com"
-    method = "GET"
-}
-
-output "client_id" {
-  value = trimprefix(regex( "[A-Za-z0-9-]*\\.apps\\.googleusercontent\\.com", data.http.webui.response_headers["X-Auto-Login"]), "253D" )
-}

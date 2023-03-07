d7c8a50ce4f412f16f1d69268e012407f5ce42a2
Aaron Strong <astrong@Burwood.com>
2023-03-07 15:04:04 -0600

    add api to cloud build

--- environment/bootstrap/apis.tf
+++ environment/bootstrap/apis.tf
@@ -7,5 +7,5 @@ resource "google_project_service" "apis" {
 variable "services" {
   description = "List of API services to enable in the Cloud Build project"
   type        = list(string)
-  default     = ["dlp.googleapis.com"]
+  default     = ["dlp.googleapis.com", "accesscontextmanager.googleapis.com"]
 }

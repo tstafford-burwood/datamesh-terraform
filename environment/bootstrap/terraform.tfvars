4c972d9aba2b74b7e5c5de9b5efe5d95b1d22e91
Aaron Strong <astrong@Burwood.com>
2023-03-13 15:22:40 -0500

    update

---
+++ environment/bootstrap/terraform.tfvars
@@ -0,0 +1,6 @@
+org_id                     = "575228741867"
+cloudbuild_service_account = "62218100388@cloudbuild.gserviceaccount.com"
+billing_account_id         = "01EF01-627C10-7CD2DF"
+
+git_repo_url = "https://github.com/Burwood/terraform-gcp-sde"
+git_path     = "cloudbuild/foundation/cloudbuild-sde-apply.yaml"
--- environment/bootstrap/trigger.tf
+++ environment/bootstrap/trigger.tf
@@ -1,43 +1,50 @@
-resource "google_cloudbuild_trigger" "bootstrap_trigger" {
+resource "google_cloudbuild_trigger" "bootstrap" {
   project = var.project_id
   name    = "bootstrap-trigger"
 
-  disabled = true
-  filename = "cloudbuild/foundation/cloudbuild-sde-apply.yaml"
+  source_to_build {
+    uri       = var.git_repo_url
+    ref       = var.git_ref
+    repo_type = var.git_repo_type
+  }
 
-  github {
-    owner = var.github_owner
-    name  = var.github_repo_name
-    push {
-      invert_regex = false
-      branch       = var.branch_name
-    }
+  git_file_source {
+    path      = var.git_path
+    repo_type = var.git_repo_type
+    revision  = var.git_ref
+    uri       = var.git_repo_url
   }
 
   substitutions = {
     _BUCKET = google_storage_bucket.tfstate.name
     _PREFIX = "foundation"
-    _TAG    = "1.3.6"
+    _TAG    = "1.4"
   }
 }
-# -------------------------------------------------------
-# VARIABLES
-# -------------------------------------------------------
 
-variable "github_owner" {
-  description = "GitHub Organization Name"
+variable "git_repo_url" {
+  description = "URL of the repository where the module code will be stored.  Used by Cloud Build to retrieve the source code and build modules.  If you want to use the open source repository, this can be set to https://github.com/GoogleCloudPlatform/rad-lab."
   type        = string
-  default     = "Burwood"
 }
 
-variable "github_repo_name" {
-  description = "Name of GitHub Repo"
+variable "git_ref" {
+  description = "What ref should be built by the Cloud Build trigger."
   type        = string
-  default     = "terraform-google-sde"
+  default     = "refs/heads/base-1"
 }
 
-variable "branch_name" {
-  description = "Regex matching branches to build. Exactly one a of branch name, tag, or commit SHA must be provided. The syntax of the regular expressions accepted is the syntax accepted by RE2 and described at https://github.com/google/re2/wiki/Syntax"
+variable "git_repo_type" {
+  description = "What the type of repository is.  Can only contain GITHUB, CLOUD_SOURCE_REPOSITORIES or UNKNOWN."
   type        = string
-  default     = "^base-1$"
+  default     = "GITHUB"
+
+  validation {
+    condition     = var.git_repo_type == "GITHUB" || var.git_repo_type == "CLOUD_SOURCE_REPOSITORIES" || var.git_repo_type == "UNKNOWN"
+    error_message = "Git repo type can only be set to GITHUB, CLOUD_SOURCE_REPOSITORIES or UNKNOWN."
+  }
+}
+
+variable "git_path" {
+  description = "The path of the file, with the repo root as the root of the path."
+  type        = string
 }

2046e8f702c2c0a4e6b85f9d304efaa442e136a6
Aaron Strong <astrong@Burwood.com>
2023-05-23 16:42:03 -0500

    change providers

--- environment/foundation/vpc-sc/backend.tf
+++ environment/foundation/vpc-sc/backend.tf
@@ -1,16 +1,20 @@
-// BACKEND AND REQUIRED PROVIDERS BLOCK
-
 terraform {
   backend "gcs" {}
-  required_version = ">= 1.1.0"
+  required_version = ">= 0.13.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.20.0" # tftest
+      version = "< 5.0, >= 3.83"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.20.0" # tftest
+      version = "< 5.0, >= 3.45"
     }
+  }
+  provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-project-factory/v13.0.0"
+  }
+  provider_meta "google-beta" {
+    module_name = "blueprints/terraform/terraform-google-project-factory/v13.0.0"
   }
 }

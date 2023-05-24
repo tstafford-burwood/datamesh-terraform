ece6770b1cb36d3989e7fe94e3c0a3664a717c54
Aaron Strong <astrong@Burwood.com>
2023-05-24 13:20:58 -0500

    update

--- environment/deployments/researcher-projects/vpc-sc/regular_service_perimeter.tf
+++ environment/deployments/researcher-projects/vpc-sc/regular_service_perimeter.tf
@@ -12,7 +12,8 @@ module "regular_service_perimeter_1" {
 
   # Grant the service accounts, like cloud build, to access project through perimeter
   # Note: To remove an access level, first remove the binding between perimeter and the access level. IE, remove the access module from the access level
-  access_levels = local.access_levels # change value to [local.fdn_sa] to remove binding from access_level_users
+  #access_levels = local.access_levels # change value to [local.fdn_sa] to remove binding from access_level_users
+  access_levels = [module.access_level_users[0].name]
 
   restricted_services     = ["storage.googleapis.com", "run.googleapis.com", "bigquery.googleapis.com", "bigtable.googleapis.com", "cloudbuild.googleapis.com", "storage.googleapis.com", "aiplatform.googleapis.com", "sqladmin.googleapis.com", "pubsub.googleapis.com", "container.googleapis.com", "artifactregistry.googleapis.com"]
   vpc_accessible_services = ["notebooks.googleapis.com", "compute.googleapis.com", "storage.googleapis.com", "artifactregistry.googleapis.com", "logging.googleapis.com", "monitoring.googleapis.com"]

9a98e96607e7a8123dafd61067ca21dc511e2a89
Aaron Strong <astrong@Burwood.com>
2023-05-24 13:56:10 -0500

    fix outputs

--- modules/vpc-sc/outputs.tf
+++ modules/vpc-sc/outputs.tf
@@ -1,9 +1,9 @@
 output "name" {
   description = "Description of the AccessLevel and its use. Does not affect behavior."
-  value       = split("/", local.output_name)[3]
+  value       = module.access_level_members.name
 }
 
 output "name_id" {
   description = "The fully-qualified name of the Access Level. Format: accessPolicies/{policy_id}/accessLevels/{name}"
-  value       = local.output_name
+  value       = module.access_level_members.name_id
 }

4007cc7e119d5ba9d030e10affe92f115565a4f5
Aaron Strong <astrong@Burwood.com>
2023-05-31 13:16:39 -0500

    remove environment values

--- environment/foundation/constants/constants.tf
+++ environment/foundation/constants/constants.tf
@@ -5,20 +5,20 @@ locals {
   constants = {
 
     // DOMAIN INFORMATION
-    automation_project_id      = "aaron-cb-sde" # Project ID that hosts Cloud Build
-    billing_account_id         = "01EF01-627C10-7CD2DF"             
-    cloudbuild_service_account = "213453898789@cloudbuild.gserviceaccount.com" # Cloud Build
-    org_id                     = "575228741867"
-    sde_folder_id              = "235621106107"
-    terraform_state_bucket     = "terraform-state-ba1e86bdae503212"
+    automation_project_id      = "<PROJECT_ID>" # Project ID that hosts Cloud Build
+    billing_account_id         = "000000-111111-222222"             
+    cloudbuild_service_account = "<PROJECT_NUMBER>@cloudbuild.gserviceaccount.com" # Cloud Build
+    org_id                     = "012345678901" # gcloud organizations list
+    sde_folder_id              = "012345678901"
+    terraform_state_bucket     = "<TERRAFORM_STATE_BUCKET>"
 
     // USERS & GROUPS TO ASSIGN TO THE FOUNDATION PROJECTS
     // format: `user:user1@client.edu`, `group:admins@client.edu`, or `serviceAccount:my-app@appspot.gserviceaccount.com`
-    ingress-project-admins = ["group:gcp_security_admins@prorelativity.com"]
-    image-project-admins   = ["group:gcp_security_admins@prorelativity.com"]
-    data-lake-admins       = ["group:gcp_security_admins@prorelativity.com"]
-    data-lake-viewers      = ["group:gcp_security_admins@prorelativity.com"]
-    data-ops-admins        = ["group:gcp_security_admins@prorelativity.com"]
+    ingress-project-admins = ["group:gcp_security_admins@example.com"]
+    image-project-admins   = ["group:gcp_security_admins@example.com"]
+    data-lake-admins       = ["group:gcp_security_admins@example.com"]
+    data-lake-viewers      = ["group:gcp_security_admins@example.com"]
+    data-ops-admins        = ["group:gcp_security_admins@example.com"]
 
     // Default Location
     default_region = "us-central1"
@@ -28,7 +28,7 @@ locals {
     environment = {
       # <branch_name> = <environment_value>
       # Example: main = "prod"
-      northwestern-prep-2 = "prod"
+      main = "prod"
     }
   }
 }
